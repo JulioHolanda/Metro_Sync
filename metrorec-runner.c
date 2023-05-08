c978e3b26d5a9dde200935c76ec35fe9c880b616
JulioHolanda <juliophc@msn.com>
2023-05-08 15:59:32 -0300

    suposta main 1/2, 1/3, 1/4.

--- metrorec-runner.c
+++ metrorec-runner.c
@@ -30,33 +30,74 @@ int main(void)
         //
         // create one station
         //
+        struct estacao station;
+        estacao_init(&station);
 
-	
+
         //
         // create a number of passengers
         // each passenger is a thread
         //
+        int qtd_passageiros = 6;
+
+        pthread_t passageiros[qtd_passageiros];
+        int t[qtd_passageiros];
+
+        for(int i=0; i<qtd_passageiros; i++){
+                t[i] = pthread_create(&passageiros[i], NULL, passageiros_thread, &station);
+                
+                if(t[i]){
+                        fprintf(stderr, "Error - pthread create() return code: %d\n", t[i]);
+                        exit(EXIT_FAILURE);
+                }
+        }
         
-        
+
         //
         // loop to create as many car as necessary to board all passengers
         //
-
+        for (int i=0; i<3; i++){
+                
                 //
                 // create only one car with a number of free seats
                 // this car is associated to a thread
                 //
+                struct vagao_args vagao_struct;
+                int qtd_assentos = qtd_passageiros / (1+i);
+
+                vagao_struct.estacao = &station;
+                vagao_struct.assentos_livres = qtd_assentos; 
+
+                pthread_t vagao;
+                int v = pthread_create(&vagao, NULL, &vagao_thread, &vagao_struct);
+                if (v){
+                        fprintf(stderr, "Error -pthread_create() return code: %d\n", v);
+                        exit(EXIT_FAILURE);
+                }
+                
 
                 //
                 // define the number of passenger to reap
                 // minimum between number of free seats and passengers at station still waiting to board
                 //
+                int passageiros_reap;
+                if(qtd_assentos < qtd_passageiros){
+                        passageiros_reap = qtd_assentos;
+                        qtd_passageiros -= qtd_assentos;
+                }else{
+                        passageiros_reap = qtd_passageiros;
+                }
 
                 //
                 // for each thread associated to a passenger that finished
                 // call estacao_embarque function to let the car know that the passenger is on board
                 // ATTENTION: the car can not have more passengers than the number of free seats
                 //
+                for(int i = 0; i<passageiros_reap; i++){
+                        estacao_embarque(&station);
+                }
+        
+        }
 
         return 0;
 }
